038104118951cba58a4e5267fe2667486f33f72f
DarwinLozada <darwinlozadadev@gmail.com>
2021-03-19 14:41:56 -0400

    added card and og meta tags to landing page

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,19 @@ export default function Home() {
           name="description"
           content="Explore our amazing universe through the NASA's Astronomic Picture of the Day API"
         />
+        <meta
+          name="og:description"
+          content="Explore our amazing universe through the NASA's Astronomic Picture of the Day API"
+        />
+        <meta property="og:ouruniverse" content="Ouruniverse" />
+        <meta property="og:title" content="Our Universe" />
+        <meta property="og:type" content="website" />
         <meta name="author" content="Darwin Lozada" />
         <meta name="robots" content="index" />
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="og:image" content="https://i.imgur.com/K1ZGEcc.png" />
+        <meta property="og:url" content="https://our-universe.vercel.app/" />
+
         <title>Our Universe - Discover</title>
         <link
           rel="preconnect"
